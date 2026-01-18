107019a7ca707b364ed7ef432ab82d8c0a57f3a8
Tim Hykes <tim@timhykes.com>
2026-01-17 23:30:11 -0500

    chore: add production config for timhykes.com

---
+++ site/config/config.production.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'debug' => false, // Disable debug mode in production for security
+    'url'   => 'https://timhykes.com', // Enforce the primary domain
+    'panel' => [
+        'install' => false // Disable panel installation in production
+    ]
+];
