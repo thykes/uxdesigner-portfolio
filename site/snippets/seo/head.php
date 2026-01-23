9ca2033cb5d1271cbf2745d37b2a00ee2af24348
Tim Hykes <tim@timhykes.com>
2026-01-23 17:51:12 -0500

    Add Person Schema to About page

--- site/snippets/seo/head.php
+++ site/snippets/seo/head.php
@@ -49,3 +49,39 @@ $seoImage = $page->cover()->toFile() ? $page->cover()->toFile()->url() : ($site-
 ], JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE) ?>
 </script>
 <?php endif ?>
+
+<?php if ($page->template() == 'about'): ?>
+<script type="application/ld+json">
+<?= json_encode([
+    '@context' => 'https://schema.org',
+    '@type' => 'Person',
+    '@id' => $page->url() . '#person',
+    'name' => $page->schema_name()->isNotEmpty() ? $page->schema_name()->value() : $site->title()->value(),
+    'alternateName' => $page->schema_alternate_name()->value(),
+    'url' => $site->url(),
+    'image' => $page->schema_image()->toFile() ? $page->schema_image()->toFile()->url() : ($page->cover()->toFile() ? $page->cover()->toFile()->url() : ''),
+    'description' => $page->intro()->value(),
+    'jobTitle' => $page->schema_job_title()->value(),
+    'worksFor' => $page->schema_works_for_name()->isNotEmpty() ? [
+        '@type' => 'Organization',
+        'name' => $page->schema_works_for_name()->value(),
+        'sameAs' => $page->schema_works_for_url()->value()
+    ] : null,
+    'alumniOf' => $page->schema_alumni_name()->isNotEmpty() ? [
+        [
+            '@type' => 'EducationalOrganization',
+            'name' => $page->schema_alumni_name()->value(),
+            'sameAs' => $page->schema_alumni_url()->value()
+        ]
+    ] : [],
+    'birthDate' => $page->schema_birth_date()->isNotEmpty() ? $page->schema_birth_date()->toDate('Y-m-d') : null,
+    'birthPlace' => $page->schema_birth_place()->isNotEmpty() ? [
+        '@type' => 'Place',
+        'name' => $page->schema_birth_place()->value()
+    ] : null,
+    'knowsAbout' => $page->schema_knows_about()->isNotEmpty() ? $page->schema_knows_about()->split(',') : [],
+    'award' => $page->schema_award()->isNotEmpty() ? $page->schema_award()->split(',') : [],
+    'sameAs' => $page->schema_same_as()->toStructure()->count() > 0 ? $page->schema_same_as()->toStructure()->map(fn($item) => $item->url()->value())->values()->toArray() : []
+], JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE) ?>
+</script>
+<?php endif ?>
